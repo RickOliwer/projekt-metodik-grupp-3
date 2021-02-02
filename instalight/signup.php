633617127968682096af4e784f9bb984a646b62c
Rick <rickoliwer@gmail.com>
2021-02-02 13:13:19 +0100

    update

--- instalight/signup.php
+++ instalight/signup.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once 'header.php';
+    //require_once 'header.php';
     require_once 'function.php';
     if(isset($_POST['submit-signup'])){
     
@@ -13,12 +13,16 @@
 
         header('Location: index.html');
     }
-
-
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
 
 <body>
     <div class="bg-signup">
